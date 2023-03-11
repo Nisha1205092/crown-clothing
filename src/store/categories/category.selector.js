11f41382882917618bc749304ea5bde36384d940
Amina Rahman Nisha <aminarahman01@gmail.com>
2023-03-11 21:53:41 +0600

    added reselect---memoized selectCategoriesMap

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,11 +1,20 @@
-export const selectCategoriesMap = (state) => {
-    console.log('state inside selectCategoriesMap ', state.categories.categories);
-    
-    return state.categories.categories.reduce((acc, category) => {
+import { createSelector } from "reselect";
+
+const selectCategoriesReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+    [selectCategoriesReducer], 
+    (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories], 
+    (categories) => categories.reduce((acc, category) => {
         const { title, items } = category;
         acc[title.toLowerCase()] = items;
         return acc;
     }, {})
-}
+)
+
 // selectors perform transformation operation on
 // basic data fetched from APIs.  
