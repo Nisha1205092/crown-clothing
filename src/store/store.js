7079beee5d9b62ce739c03e6369fc2d491bbcb0b
Amina Rahman Nisha <aminarahman01@gmail.com>
2023-03-11 16:20:59 +0600

    added our own lodder middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,21 @@
 import { compose, legacy_createStore as createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
-const middlewares = [logger];
+const loggrMiddleware = (store) => (next) => (action) => {
+    if(!action.type) {
+        return next(action);
+    }
+    console.log('type: ', action.type);
+    console.log('payload: ', action.payload);
+    console.log('current state: ', store.getState());
+
+    next(action);
+
+    console.log('next state: ', store.getState());
+}
+
+const middlewares = [loggrMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middlewares))
 
