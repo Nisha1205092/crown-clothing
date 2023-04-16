6798fd27ad0ad8228204bc8b83c87c414a4d70c0
Amina Rahman Nisha <aminarahman01@gmail.com>
2023-04-16 21:24:40 +0600

    converting sagas from js to ts

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,25 +1,25 @@
-import { all, call, put, takeLatest } from "redux-saga/effects";
+import { all, call, put, takeLatest } from "typed-redux-saga";
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import { fetchCategoriesFailed, fetchCategoriesSuccess } from "./category.action";
 import { CATEGORY_ACTION_TYPE } from "./category.types";
 
 export function* fetchCategoriesAsync() {
     try {
-        const categoriesArray = yield call(getCategoriesAndDocuments); // async
-        yield put(fetchCategoriesSuccess(categoriesArray));
+        const categoriesArray = yield* call(getCategoriesAndDocuments); // async
+        yield* put(fetchCategoriesSuccess(categoriesArray));
     } catch (error) {
-        yield put(fetchCategoriesFailed(error));
+        yield* put(fetchCategoriesFailed(error as Error));
     }
 };
 
 // generators listen to actions
 export function* onFetchCategories() {
     // "take" is where we receive "actions"
-    yield takeLatest(CATEGORY_ACTION_TYPE.FETCH_CATEGORIES_START, fetchCategoriesAsync);
+    yield* takeLatest(CATEGORY_ACTION_TYPE.FETCH_CATEGORIES_START, fetchCategoriesAsync);
 
 }
 
 export function* categoriesSaga() { // holds all sagas related to Categories
     // works like an accumulator
-    yield all([call(onFetchCategories)]);
+    yield* all([call(onFetchCategories)]);
 }
