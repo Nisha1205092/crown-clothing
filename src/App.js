5082d321b3ef8981669fdc95a9e441b2191f5260
Amina Rahman Nisha <aminarahman01@gmail.com>
2023-05-01 00:36:08 +0600

    added code splitting using dynamic imports, lazy, Suspenseare used

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from 'react-router-dom';
-import { useEffect } from "react";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/shop/shop.component";
-import Checkout from "./routes/checkout/checkout.component";
+import { useEffect, lazy, Suspense } from "react";
 import { checkUserSession } from "./store/user/user.action";
 import { useDispatch } from "react-redux";
+import Spinner from "./components/spinner/spinner.component";
+
+const Navigation = lazy(() => import("./routes/navigation/navigation.component"));
+const Authentication = lazy(() => import("./routes/authentication/authentication.component"));
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -15,15 +17,17 @@ const App = () => {
     dispatch(checkUserSession())
   }, [dispatch])
 
-  return ( 
-    <Routes>
-      <Route path="/" element={<Navigation />} >
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+  return (
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />} >
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
