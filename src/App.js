f9c7590a82596f56c51327e86079c8cbe6bb346f
Amina Rahman Nisha <aminarahman01@gmail.com>
2023-05-05 00:17:38 +0600

    added margin and changed width of Directory component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useEffect, lazy, Suspense } from "react";
 import { checkUserSession } from "./store/user/user.action";
 import { useDispatch } from "react-redux";
 import Spinner from "./components/spinner/spinner.component";
+// import { GlobalStyle } from "./global.styles";
 
 const Navigation = lazy(() => import("./routes/navigation/navigation.component"));
 const Authentication = lazy(() => import("./routes/authentication/authentication.component"));
--- src/components/directory/directory.styles.tsx
+++ src/components/directory/directory.styles.tsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 export const DirectoryContainer = styled.div`
-    width: 100%;
+    width: 95%;
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
     padding-top: 40px;
+    margin: auto;
 `;
